234eeaaa42821236217c6145c7f72ea94015a524
David Souther <davidsouther+github@gmail.com>
2023-11-12 15:29:55 -0500

    ci

--- simulator/src/test/builder.ts
+++ simulator/src/test/builder.ts
@@ -16,7 +16,6 @@ import {
   TestClearEchoInstruction,
   TestCompoundInstruction,
   TestEchoInstruction,
-  TestLoadInstruction,
   TestLoadROMInstruction,
   TestOutputInstruction,
   TestOutputListInstruction,
